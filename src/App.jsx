96be162486861be551ec4548044055ecddc69561
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-12-28 20:18:20 +0330

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Qrcode from "./components/Qrcode";
 
-
 function App() {
   return (
-    <main className="bg-gray-200  h-screen flex justify-center items-center">
+    <main className="bg-[#D5E1EF]  h-screen flex justify-center items-center">
       <Qrcode />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Qrcode.jsx
+++ src/components/Qrcode.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function qrcode() {
   return (
-    <div >
-      <div className=' bg-white max-w-sm p-4 rounded-lg'>
+    <div>
+      <div className=" bg-white w-[20rem] h-[31.0625rem] p-4 rounded-lg">
         <div className="">
-          <img src="../images/image-qr-code.png" className=' rounded-lg' />
+          <img src="../images/image-qr-code.png" className=" rounded-lg" />
         </div>
-          <h1 className='font-bold p-3 text-2xl ml-4'>Improve your front-end skills by building projects</h1>
-          <p className='text-gray-600 p-3'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        <h1 className="font-bold text-[1.375rem] ml-4">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="text-[#7D889E] text-[0.9375rem] p-3">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
       </div>
     </div>
-  )
+  );
 }
